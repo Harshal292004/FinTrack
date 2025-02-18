8518252adbad5ff373bc33df4cdb29d83d34b3cf
Harshal292004 <malaniharshal95@gmail.com>
2025-02-18 09:08:54 +0530

    Added the dashboard

---
+++ src/app/(pages)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Hero from '@/components/dashboard/Hero'
+const page = () => {
+  return (
+    <Hero></Hero>  
+    )
+}
+
+export default page
---
+++ src/components/dashboard/Hero.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { TableDemo } from './RecentTransactions'
+import { Component } from './MonthlyExpenseChart'
+import { Button } from '../ui/button'
+const Hero = () => {
+  return (
+    <div className='w-full h-full mt-28'>
+        {/*grid 1 */}
+        {/*today's total expense*/}
+        <div className='flex flex-col rounded-sm border border-white'>
+            <h1>Your today's Expenses</h1>
+            <span>300 $</span>
+        </div>
+        {/*what ai says about it? */}
+        <div className='flex flex-col rounded-sm border border-white'>
+            <h1>What Fin has to say about this?</h1>
+            <Button className=''>Ask Fin</Button>
+        </div>
+        {/*grid 2*/}
+        {/*Monthly chart of expenses*/}
+        <Component></Component>
+        {/*Recent transactions*/}
+        <TableDemo></TableDemo>
+
+        {/*button at the button right corner for adding deleting editing transactions*/}
+    </div>
+  )
+}
+
+export default Hero
